69544f8824a937958481524322762c0ef009d9bb
Alex Legio <legiosys@gmail.com>
2024-03-04 11:19:23 +0300

    Чуть оптимизорвал ассайн

--- PopugJira/PopugJira.Tracker/Services/TaskAssigner.cs
+++ PopugJira/PopugJira.Tracker/Services/TaskAssigner.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Concurrent;
+using Microsoft.EntityFrameworkCore;
 using PopugJira.Auth.Contracts;
 using PopugJira.Tracker.Db;
+using PopugJira.Tracker.Models;
 
 namespace PopugJira.Tracker.Services;
 
 public class TaskAssigner(TrackerDbContext db)
 {
-
+    private Popug[]? _inMemoryPopugs;
+    
     public async Task<Guid> GetAssignee()
     {
-        var popugs = await db.Popugs.AsNoTracking()
+        _inMemoryPopugs ??= await db.Popugs.AsNoTracking()
             .Where(x => x.Position.Equals(PopugPositionsEnum.Worker))
             .ToArrayAsync();
 
-        var selectedPopug = BigBrotherSelectPopugMethod(popugs.Length);
-        return popugs[selectedPopug].Id;
+        if (_inMemoryPopugs.Length == 0)
+            throw new Exception("Нет попугов, на которых можно назначить задачу");
+            
+        var selectedPopug = BigBrotherSelectPopugMethod(_inMemoryPopugs.Length);
+        return _inMemoryPopugs[selectedPopug].Id;
     }
 
     private static int BigBrotherSelectPopugMethod(int popugsCount) => Random.Shared.Next(0, popugsCount - 1);
